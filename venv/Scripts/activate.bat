deb9f1423bac28c3eec47377db53b92b7854c316
umts12 <umts2358@naver.com>
2020-12-03 19:02:37 +0900

    새로운 컴퓨터에 오다!

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\umts1202\Desktop\CoCo\DanDalu\venv
+set VIRTUAL_ENV=C:\Users\user\Desktop\AI_Study\DanDalu\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
