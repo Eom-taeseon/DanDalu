b55dc82434f155eb4cae16c4bca9d98c25731b23
umts12 <umts2358@naver.com>
2020-12-04 09:47:16 +0900

    20.12.04 블로그 메인 컨테츠 구성 완료 데이터베이스 시작

--- DanDalu/urls.py
+++ DanDalu/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import blogapp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', blogapp.views.index, name='index'),
+    path('blogMain/', blogapp.views.blogMain, name='blogMain')
 ]
